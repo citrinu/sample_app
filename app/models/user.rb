37422d1e208a67cff276a27aee1172efda0d2548
Citrinu <citrinu@lemonfarm.com>
2018-10-08 15:44:37 -0400

    added tests for: in/valid user emails, length checks, uniqueness signup page test email check using regex constant added index for users/email for faster lookup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+
+  #constant
+  #allows invalid addresses that contain consecutive dots
+  #pls fix
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name, presence: true, length: {maximum: 50}
-  validates :email, presence: true, length: {maximum: 255}
+
+    #checks if the given email format matches the regex
+  validates :email, presence: true, length: {maximum: 255},
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+
 
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -36,4 +36,33 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "accept valid emails"  do
+    #creates an array of valid emails
+    valid_emails = %w[thisworks@example.com yep@mail.COM forest@green.mail.com]
+
+    #iterates through the array
+    valid_emails.each do |valid_email|
+      @user.email = valid_email #sets email to passed in one
+      assert @user.valid?, "#{valid_email.inspect} should be a valid email." #optional arg provides an error message
+    end
+  end
+
+  test "reject invalid emails" do
+    invalid_email = %w[heyhey@email,com wont.work@w@email.com meme@meme+meme.com]
+
+    invalid_email.each do |invalid_emails|
+      @user.email = invalid_emails
+      assert_not @user.valid?, "#{invalid_email.inspect} should be invalid"
+    end
+  end
+
+  test "emails should be unique" do
+    duplicate_user = @user.dup #creates a user
+    duplicate_user.email = @user.email.upcase # for case sensivity
+    @user.save #saves it to the db
+    assert_not duplicate_user.valid?
+  end
+
+
+
 end
