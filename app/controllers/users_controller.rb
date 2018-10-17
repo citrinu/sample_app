5ad02f2c230203f697553aafc8e8483699f04961
Citrinu <citrinu@lemonfarm.com>
2018-10-17 15:16:37 -0400

    Added strong parameters  when creating a new user redefined create action added private method user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,20 @@ class UsersController < ApplicationController
     #debugger
   end
 
+  #defining the create action from the user resource
+  def create
+      @user = User.new(user_params)
+      if @user.save
+
+      else
+        render 'new'
+      end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
